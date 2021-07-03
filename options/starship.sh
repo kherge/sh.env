74bc068e89015a96b594d38846855e8c9a0bb1bc
Kevin Herrera <kevin@herrera.io>
2021-07-03 00:11:26 +0000

    Adding a Starship option.

---
+++ options/starship.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+################################################################################
+# Starship Prompt                                         https://starship.rs/ #
+#                                                                              #
+# Activates Starship if it is available in PATH.                               #
+################################################################################
+
+###
+# Initializes Starship for the supported shell.
+##
+__env_option_starship_activate()
+{
+    if command -v starship > /dev/null; then
+        if [ "$BASH_VERSION" != '' ]; then
+            eval "$(starship init bash)"
+        elif [ "$ZSH_VERSION" != '' ]; then
+            eval "$(starship init zsh)"
+        else
+            echo "env: starship: shell not supported" >&2
+            echo >&2
+        fi
+    else
+        echo "env: starship: command not available" >&2
+        echo >&2
+    fi
+}
+
+###
+# Does nothing.
+##
+__env_option_starship_disable()
+{
+    : # Do nothing.
+}
+
+###
+# Does nothing.
+##
+__env_option_starship_enable()
+{
+    # Should probably be the last thing loaded.
+    __ENV_PRIORITY=99
+}
