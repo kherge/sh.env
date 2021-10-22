6a98522eabefef041e7888a72fef73689bf586ae
Kevin Herrera <kevin@herrera.io>
2021-10-21 21:36:29 -0700

    Better handling of unsupported shells.

--- options/sdkman.sh
+++ options/sdkman.sh
@@ -12,8 +12,17 @@
 ##
 __env_option_sdkman_activate()
 {
+    # Make sure we are in BASH or ZSH.
+    if [ "$BASH_VERSION" = '' ] && [ "$ZSH_VERSION" = '' ]; then
+        __env_debug "sdkman: BASH or ZSH is required"
+
+        return 1
+    fi
+
     # Load the shell script.
     . "$HOME/.sdkman/bin/sdkman-init.sh"
+
+    return $?
 }
 
 ###
@@ -42,7 +51,19 @@ __env_option_sdkman_enable()
             return 1
         fi
 
-        if ! eval "$(curl -s "https://get.sdkman.io?rcupdate=false")"; then
+        local SHELL=
+
+        if command -v bash > /dev/null; then
+            SHELL=bash
+        elif command -v zsh > /dev/null; then
+            SHELL=zsh
+        else
+            __env_err "sdkman: BASH or ZSH is required"
+
+            return 1
+        fi
+
+        if ! curl -s "https://get.sdkman.io?rcupdate=false" | "$SHELL"; then
             __env_err "sdkman: could not be installed"
 
             return 1
